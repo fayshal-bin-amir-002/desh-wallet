21b9baf292c5db61cd31a222ad4ac4e3b6c6cbf3
fayshal69 <foyshalbinamir@gmail.com>
2024-07-16 00:32:00 +0600

    add: banner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
                     </div>
                     <div className="drawer-side">
                         <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
-                        <ul className="menu bg-base-100 text-base-content bg-opacity-100 min-h-full w-80 p-4 space-y-2 text-base ">
+                        <ul className="menu bg-base-100 text-base-content bg-opacity-100 min-h-full w-80 p-4 space-y-2 text-base">
                             <li className="bg-base-200 rounded-lg"><Link to="">Overviews</Link></li>
                             <li className="bg-base-200 rounded-lg"><Link to="">Transitions</Link></li>
                         </ul>
